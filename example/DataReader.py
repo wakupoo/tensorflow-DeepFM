cf1d3028c85fcccb478ece2939cdafac39f5bc7f
wakupoo <wwkupoo@163.com>
2018-05-03 12:45:04 +0800

    Update DataReader.py

--- example/DataReader.py
+++ example/DataReader.py
@@ -21,26 +21,27 @@ class FeatureDictionary(object):
         self.gen_feat_dict()
 
     def gen_feat_dict(self):
-        if self.dfTrain is None:
+        if self.dfTrain is None:#读取dfTrain
             dfTrain = pd.read_csv(self.trainfile)
         else:
             dfTrain = self.dfTrain
-        if self.dfTest is None:
+        if self.dfTest is None:#读取dfTest
             dfTest = pd.read_csv(self.testfile)
         else:
             dfTest = self.dfTest
         df = pd.concat([dfTrain, dfTest])
-        self.feat_dict = {}
+        self.feat_dict = {}#特征的index
         tc = 0
         for col in df.columns:
             if col in self.ignore_cols:
                 continue
             if col in self.numeric_cols:
                 # map to a single index
-                self.feat_dict[col] = tc
+                self.feat_dict[col] = tc#给特征index
                 tc += 1
             else:
                 us = df[col].unique()
+                #非连续值考虑one-hot后的情况，[0,1,2,0],us=3,tc=6,feat_dict[col]=((0,6)(1,7)(2,8))
                 self.feat_dict[col] = dict(zip(us, range(tc, len(us)+tc)))
                 tc += len(us)
         self.feat_dim = tc
@@ -51,15 +52,15 @@ class DataParser(object):
         self.feat_dict = feat_dict
 
     def parse(self, infile=None, df=None, has_label=False):
-        assert not ((infile is None) and (df is None)), "infile or df at least one is set"
+        assert not ((infile is None) and (df is None)), "infile or df at least one is set"#必须传文件路径或者dataframe
         assert not ((infile is not None) and (df is not None)), "only one can be set"
-        if infile is None:
+        if infile is None:#没有输入文件路径，则取dataframe
             dfi = df.copy()
-        else:
+        else:#否则从输入文件路径中读取
             dfi = pd.read_csv(infile)
         if has_label:
-            y = dfi["target"].values.tolist()
-            dfi.drop(["id", "target"], axis=1, inplace=True)
+            y = dfi["target"].values.tolist()#如有label，获取label
+            dfi.drop(["id", "target"], axis=1, inplace=True)#去读id和label
         else:
             ids = dfi["id"].values.tolist()
             dfi.drop(["id"], axis=1, inplace=True)
@@ -67,14 +68,14 @@ class DataParser(object):
         # dfv for feature value which can be either binary (1/0) or float (e.g., 10.24)
         dfv = dfi.copy()
         for col in dfi.columns:
-            if col in self.feat_dict.ignore_cols:
+            if col in self.feat_dict.ignore_cols:#删除不需要的特征
                 dfi.drop(col, axis=1, inplace=True)
                 dfv.drop(col, axis=1, inplace=True)
                 continue
             if col in self.feat_dict.numeric_cols:
                 dfi[col] = self.feat_dict.feat_dict[col]
             else:
-                dfi[col] = dfi[col].map(self.feat_dict.feat_dict[col])
+                dfi[col] = dfi[col].map(self.feat_dict.feat_dict[col])#离散特征
                 dfv[col] = 1.
 
         # list of list of feature indices of each sample in the dataset
